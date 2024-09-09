6b0bd620ea3b60d93be9f54fdd63a64b6c3d3c3d
kajalsoni-design <143927553+kajalsoni-design@users.noreply.github.com>
2024-09-09 14:56:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import HeroSection from './components/Hero';
+import Navigation from './components/Navigation';
+
+
+const App = ()=> {
+  return (
+    <>
+  <Navigation></Navigation>
+  <HeroSection></HeroSection>
+    </>
+  )
+}
+
+export default App;
